12b16aaaa7696340bcd2fbbddc6fd301fbab1cd4
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-05 22:46:53 +0300

    Task31

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,46 @@
 export default{
 data(){
 return{
-    isSmeshariki: [1, 2, 3],
+    isSmeshariki: [
+    {href: '1.html',text: 'Polina Shapkina',},
+    {href: '2.html',text: 'Polina Shapkina',},
+    {href: '3.html',text: 'Polina Shapkina',},
+    ],
+    products: [
+    {
+        name: 'product1',
+        price: 100,
+        quantity: 5
+    },
+    {
+        name: 'product2',
+        price: 200,
+        quantity: 4
+    },
+    {
+        name: 'product3',
+        price: 300,
+        quantity: 3
+    },
+    ]
 }
 }
 }
 </script>
 
 <template>
-<div v-for="elem in isSmeshariki">
-<p>{{ elem }}</p>
-<p class="divider"> </p>
-</div>
-<br>
-<ul v-for="elem in isSmeshariki">
-    <li>{{ elem }}</li>
-    <li class="divider"> </li>
+<ul class="styled">
+    <li v-for="link in isSmeshariki"><a isSmeshariki="{{ link.isSmeshariki }}">{{ link.text }}</a></li>
 </ul>
+<br>
+<br>
+<table>
+    <tr v-for="col in products">
+    <td>{{ col.name }}</td>
+    <td>{{ col.price }}</td>
+    <td>{{ col.quantity }}</td>
+    </tr>
+</table>
 </template>
 
 <style >
