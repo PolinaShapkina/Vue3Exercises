5c79ce11fee4e6199322302f9d02714782aeb53b
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-05 14:48:12 +0300

    Task8

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,14 @@
 export default{
 data(){
 return{
-    arr: ['x', 'y', 'z'],
-    arr1: [1, 2, 3],
+    obj: {x: 1, y: 2, z: 3},
 }
 }
 }
 </script>
 
 <template>
-<div>{{ arr[0] }}</div>
-<div>{{ arr[1] }}</div>
-<div>{{ arr[2] }}</div>
-<div>{{ arr1[0] + arr1[1] + arr1[2] }}</div>
+<div>{{ obj.x + obj.y + obj.z }}</div>
 </template>
 
 <style >
