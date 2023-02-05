317156339d528bb751d8fd2fae846961d0a96f77
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-05 21:39:25 +0300

    Task22

--- src/App.vue
+++ src/App.vue
@@ -2,32 +2,15 @@
 export default{
 data(){
 return{
-    visible: true,
-    visible2: true,
-    visible3: true,
-}
-},
-methods: {
-Nyusha: function(){
-    this.visible = !this.visible;
-},
-Pin: function () {
-    this.visible2 = !this.visible2;
-},
-Krosh: function () {
-    this.visible3 = !this.visible3;
+    isSmeshariki: false,
 }
 }
 }
 </script>
 
 <template>
-<button @click="Nyusha">Nyusha</button>
-<p>{{visible ? 'Nyusha' : '!!!!'}}</p>
-<button @click="Pin">Pin</button>
-<p>{{visible2 ? 'Pin' : '!!!!'}}</p>
-<button @click="Krosh">Krosh</button>
-<p>{{visible3 ? 'Krosh' : '!!!!'}}</p>
+<p v-if="isSmeshariki">Nyusha</p>
+<p v-else>Pin</p>
 </template>
 
 <style >
