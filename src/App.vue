7ad320747e74a5e1754b0cfcb20528c846e17dc6
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-05 14:50:07 +0300

    Task9

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <script>
 export default{
 data(){
-return{
-    obj: {x: 1, y: 2, z: 3},
+
+},
+methods: {
+show: function(){
+    alert(new Date().toLocaleDateString());
 }
 }
 }
 </script>
 
 <template>
-<div>{{ obj.x + obj.y + obj.z }}</div>
+<button @click="show">date</button>
 </template>
 
 <style >
