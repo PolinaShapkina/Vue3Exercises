5412ea6b9f94820d07485eb8b3e0d00e8aaeccc7
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-05 15:33:36 +0300

    Task16

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,21 @@
 <script>
 export default{
 data(){
-return{
-    cost: '70',
-    amount: '15',
-}
-},
-computed: {
-price: function(){
-    return this.cost *  this.amount;
-}
+
 },
 methods: {
-change: function(){
-this.cost = '30'
+show: function(){
+    alert(new Date().toLocaleDateString());
 }
 }
 }
 </script>
 
 <template>
-<p>{{ cost }}</p>
-<p>{{ amount }}</p>
-<p>{{ price }}</p>
-<button @click="change">cost</button>
+<a class="inline-link-1" href="https://losyash-library.fandom.com/ru/wiki/%D0%A1%D0%BC%D0%B5%D1%88%D0%B0%D1%80%D0%B8%D0%BA%D0%B8" onclick="event.preventDefault()">Krosh</a>
+<br>
+<br>
+<button @click.once="show">Krosh</button>
 </template>
 
 <style >
