e671e18e6baf24d18720d23246c41401117aa081
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-05 22:29:02 +0300

    Task28

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,14 @@
 export default{
 data(){
 return{
-    isSmeshariki: [1, 2, 3, 4, 5],
+    isSmeshariki: ['x', 'y', 'z'],
 }
 }
 }
 </script>
 
 <template>
-<div v-for="elem in isSmeshariki">{{ elem }}</div>
-<br>
-<div v-for="elem in isSmeshariki">{{ elem*elem }}</div>
-<br>
-<ul v-for="elem in isSmeshariki">{{ elem }}</ul>
+<ul v-for="(elem, key) in isSmeshariki"> {{ key }} {{ elem }}</ul>
 </template>
 
 <style >
