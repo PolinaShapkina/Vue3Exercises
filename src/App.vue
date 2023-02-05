6db2afb0c36fcf57dab1a92be04ea2c0a48f33b3
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-05 22:49:26 +0300

    Task32

--- src/App.vue
+++ src/App.vue
@@ -2,46 +2,13 @@
 export default{
 data(){
 return{
-    isSmeshariki: [
-    {href: '1.html',text: 'Polina Shapkina',},
-    {href: '2.html',text: 'Polina Shapkina',},
-    {href: '3.html',text: 'Polina Shapkina',},
-    ],
-    products: [
-    {
-        name: 'product1',
-        price: 100,
-        quantity: 5
-    },
-    {
-        name: 'product2',
-        price: 200,
-        quantity: 4
-    },
-    {
-        name: 'product3',
-        price: 300,
-        quantity: 3
-    },
-    ]
 }
 }
 }
 </script>
 
 <template>
-<ul class="styled">
-    <li v-for="link in isSmeshariki"><a isSmeshariki="{{ link.isSmeshariki }}">{{ link.text }}</a></li>
-</ul>
-<br>
-<br>
-<table>
-    <tr v-for="col in products">
-    <td>{{ col.name }}</td>
-    <td>{{ col.price }}</td>
-    <td>{{ col.quantity }}</td>
-    </tr>
-</table>
+<p v-for="num in 30"> {{ num }}</p>
 </template>
 
 <style >
