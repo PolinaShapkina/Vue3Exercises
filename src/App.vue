94605ff645538860115ba1036b452612a5cff9bf
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-05 23:06:43 +0300

    Task35

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,47 @@
 export default{
 data(){
 return{
-    Smeshariki: [
-    {
-        id: 1,
-        name: 'Krosh',
-    },
-    {
-        id: 2,
-        name: 'Pin',
-    },
-    {
-        id: 3,
-        name: 'Nyusha',
-    },
-    ]
+    Smeshariki: ['Nyusha','Pin','Krosh']
 }
+},
+methods: {
+add: function(){
+this.Smeshariki.push('Olga');
+},
+del: function(){
+this.Smeshariki.shift();
+},
+del1: function () {
+this.Smeshariki.pop();
+},
+splice: function(){
+this.Smeshariki.splice(2, 1);
+},
+sort: function(){
+this.Smeshariki.sort();
+},
+reverse: function () {
+    this.Smeshariki.reverse();
+},
 }
 }
 </script>
 
 <template>
-<p class= "styled" v-for="prod in Smeshariki" :key='Smeshariki.id'>{{ prod.name }}</p>
+<ul>
+<li v-for="elem in Smeshariki">{{ elem }}</li>
+</ul>
+<button @click="add">1</button>
+<br>
+<button @click="del">2</button>
+<br>
+<button @click="del1">3</button>
+<br>
+<button @click="splice">4</button>
+<br>
+<button @click="sort">5</button>
+<br>
+<button @click="reverse">6</button>
 </template>
 
 <style >
