3a85e0a5ee1e84e9155f964de41276810d7964bb
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-05 15:36:21 +0300

    Task17

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,18 @@
 <script>
 export default{
 data(){
-
+return{
+    visible: true,
+    visible1: false,
+}
 },
-methods: {
-show: function(){
-    alert(new Date().toLocaleDateString());
-}
-}
 }
 </script>
 
 <template>
-<a class="inline-link-1" href="https://losyash-library.fandom.com/ru/wiki/%D0%A1%D0%BC%D0%B5%D1%88%D0%B0%D1%80%D0%B8%D0%BA%D0%B8" onclick="event.preventDefault()">Krosh</a>
-<br>
-<br>
-<button @click.once="show">Krosh</button>
+<p v-if="visible">Krosh</p>
+<p v-if="visible1">Pin</p>
+
 </template>
 
 <style >
