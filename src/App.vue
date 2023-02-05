7b08f157c1f0afd8e456dd234e554f03c2ef2613
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-05 14:56:09 +0300

    Task11

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,30 @@
 <script>
 export default{
 data(){
-
+return{
+    num1: 1,
+    num2: 2,
+    text: 'polina'
+}
 },
 methods: {
 show: function(){
-    alert(new Date().toLocaleDateString());
+    let text = this.cape(this.text)
+    alert(text);
+    // сумма чисел;
+},
+cape: function(str){
+    return str[0].toUpperCase() + str.slice(1);
 }
 }
 }
 </script>
 
 <template>
-<button  v-on:mouseover="show">наведение</button>
-<button  @click="show">нажатие</button>
+<div>{{ text }}</div>
+<div>{{ num1 + num2 }}</div>
+<button v-on:mouseover="show">наведение</button>
+<button @click="show">нажатие</button>
 </template>
 
 <style >
