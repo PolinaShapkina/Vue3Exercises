1c451ae884f50244c798934e3b6b5c456ebd0641
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-06 18:34:15 +0300

    Task38

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,10 @@ export default{
 data(){
 return{
     cssClasses: 'Smeshariki',
+    obj: {
+    done: true,
+    selected: false,
+}
 }
 }
 }
@@ -10,6 +14,7 @@ return{
 
 <template>
 <p :class="cssClasses">Smeshariki</p>
+<p :class="obj">Krosh</p>
 </template>
 
 <style >
@@ -18,6 +23,24 @@ return{
     margin: 10px;
     background: rgb(160, 204, 0);
     color: rgb(255, 255, 255);
+    font-weight: bold;
+    text-decoration: none;
+    font-size: 20px;
+}
+.selected{
+    padding: 10px;
+    margin: 10px;
+    background: rgb(234, 129, 16);
+    color: white;
+    font-weight: bold;
+    text-decoration: none;
+    font-size: 20px;
+}
+.done {
+    padding: 10px;
+    margin: 10px;
+    background: rgb(20, 247, 213);
+    color: rgb(12, 51, 245);
     font-weight: bold;
     text-decoration: none;
     font-size: 20px;
