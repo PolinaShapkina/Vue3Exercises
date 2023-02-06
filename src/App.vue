b5bcadf75d273c84bb940b7593ab61bd0ae4bf38
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-06 21:50:45 +0300

    Task48

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,21 @@
 export default{
 data(){
 return{
-    languages: [],
+    languages: '',
+    languages1: false,
+    languages2: false,
+    languages3: false,
+}
+},
+methods: {
+aaa(){
+    this.languages1 = true;
+},
+bbb() {
+    this.languages2 = true;
+},
+vvv() {
+    this.languages3 = true;
 }
 }
 }
@@ -10,18 +24,22 @@ return{
 
 <template>
 <div>
-<input type="checkbox" v-model="languages" value="Английский">
-<label>Английский</label>
+<p>На каком языке вы разговариваете?</p>
+<input name="radio" type="radio" v-model="languages" value="Русский" @click="aaa">
+<label for="">Русский</label>
 <br>
-<input type="checkbox" v-model="languages" value="Русский">
-<label>Русский</label>
+<input name="radio" type="radio" v-model="languages" value="Английский" @click="bbb">
+<label for="">Английский</label>
 <br>
-<input type="checkbox" v-model="languages" value="Испанский">
-<label>Испанский</label>
-<ol>
-<li v-for="elem in languages">{{ elem }}</li>
-</ol>
+<input name="radio" type="radio" v-model="languages" value="Украинский" @click="vvv">
+<label for="">Украинский</label>
 </div>
+<p v-if="visible">Здравствуйте</p>
+<p v-else-if="visible1">Hello</p>
+<p v-else-if="visible2">Привіт</p>
+<p v-else=''> </p>
+
+<p>ОТВЕТ: {{ languages }}</p>
 <br>
 </template>
 
