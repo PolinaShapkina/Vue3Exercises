7bd657da882063c163e7f75781170ee89bcd4d5a
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-06 21:21:57 +0300

    Task44

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,34 @@
 export default{
 data(){
 return{
-message: 'Polina',
-number: 'Shapkina',
+    num: 0,
+    res: 0,
+sum: 0,
+num1: 0, 
+num2: 0,
 }
 },
 methods: {
+Smeshariki: function () {
+    this.res = Math.sqrt(this.num);
+},
+smeshariki:function(){
+    this.sum = this.num1 + this.num2;
+}
 }
 }
 </script>
 
 <template>
-<input class="text-field__input" v-model="message">
-<p :style="{textTransform: 'uppercase'}">{{ message }}</p>
+<p>{{ res }}</p>
+<input class="text-field__input" v-model="num">
+<button @click="Smeshariki">квадратный корень</button>
 <br>
-<input class="text-field__input" v-model="number">
-<p>{{ number*number }}</p>
+<p>{{ sum }}</p>
+<input class="text-field__input" v-model=num1>
+<input class="text-field__input" v-model=num2>
+<button @click="smeshariki">сумма</button>
+<br>
 </template>
 
 <style>
