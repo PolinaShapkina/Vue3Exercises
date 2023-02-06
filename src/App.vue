23513ebc8fbe7fb1511222eae3e16432b6b20d5c
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-06 22:03:01 +0300

    Task49

--- src/App.vue
+++ src/App.vue
@@ -2,45 +2,20 @@
 export default{
 data(){
 return{
-    languages: '',
-    languages1: false,
-    languages2: false,
-    languages3: false,
-}
-},
-methods: {
-aaa(){
-    this.languages1 = true;
-},
-bbb() {
-    this.languages2 = true;
-},
-vvv() {
-    this.languages3 = true;
+    aaa: 'Москва',
 }
 }
 }
 </script>
 
 <template>
-<div>
-<p>На каком языке вы разговариваете?</p>
-<input name="radio" type="radio" v-model="languages" value="Русский" @click="aaa">
-<label for="">Русский</label>
-<br>
-<input name="radio" type="radio" v-model="languages" value="Английский" @click="bbb">
-<label for="">Английский</label>
-<br>
-<input name="radio" type="radio" v-model="languages" value="Украинский" @click="vvv">
-<label for="">Украинский</label>
-</div>
-<p v-if="visible">Здравствуйте</p>
-<p v-else-if="visible1">Hello</p>
-<p v-else-if="visible2">Привіт</p>
-<p v-else=''> </p>
-
-<p>ОТВЕТ: {{ languages }}</p>
-<br>
+<p>В каком городе вы живете?</p>
+    <select v-model="aaa">
+    <option>Алушта</option>
+    <option>Москва</option>
+    <option>Санкт-Питербург</option>
+    </select>
+    <p>{{ aaa }}</p>
 </template>
 
 <style>
