8f63d12801d77739aa5afec80f674b648eb86099
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-06 21:02:23 +0300

    Task42

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,15 @@
 export default{
 data(){
 return{
-    isValid: true,
-    isDisabled: true,
 }
 }
 }
 </script>
 
 <template>
-<p :class="{smeshariki: isValid, disable: isDisabled}">Smeshariki</p>
+<p :style="{color: 'green', background: 'yellow'}">Smeshariki</p>
+<p :style="{fontWeight: 'bold', fontStyle: 'italic'}">Krosh</p>
 </template>
 
-<style >
-p.smeshariki{
-padding: 10px;
-margin: 10px;
-background: rgb(245, 226, 15);
-color: rgb(245, 22, 22);
-font-weight: bold;
-text-decoration: none;
-}
+<style>
 </style>
