29a61cb799d7eaea9689179b27b766288fa6c494
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-06 20:52:00 +0300

    Task41

--- src/App.vue
+++ src/App.vue
@@ -2,46 +2,24 @@
 export default{
 data(){
 return{
-    cssClasses: 'Smeshariki',
-    visible: true,
-    style: {
-    hidden: true,
-}
-}
-},
-methods: {
-setDone:function(){
-this.style.hidden = false;
-},
-setTwo: function(){
-this.style.hidden = true;
-},
-smeshariki: function () {
-    this.visible = !this.visible;
+    isValid: true,
+    isDisabled: true,
 }
 }
 }
 </script>
 
 <template>
-<button @click="smeshariki">Smeshariki</button>
-<p class="Smeshariki" v-if="visible">Smeshariki</p>
-<p :class="style">Conclave</p>
-<button @click="setDone">показывает</button>
-<button @click="setTwo">прячет</button>
+<p :class="{smeshariki: isValid, disable: isDisabled}">Smeshariki</p>
 </template>
 
 <style >
-.Smeshariki {
-    padding: 10px;
-    margin: 10px;
-    background: rgb(160, 204, 0);
-    color: rgb(255, 255, 255);
-    font-weight: bold;
-    text-decoration: none;
-    font-size: 20px;
-}
-p.hidden{
-display: none;
+p.smeshariki{
+padding: 10px;
+margin: 10px;
+background: rgb(245, 226, 15);
+color: rgb(245, 22, 22);
+font-weight: bold;
+text-decoration: none;
 }
 </style>
