900c088f3bb37fc12d89b04f8aa97f1fe7a139c8
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-06 18:20:18 +0300

    Task36

--- src/App.vue
+++ src/App.vue
@@ -2,48 +2,22 @@
 export default{
 data(){
 return{
-    Smeshariki: ['Nyusha','Pin','Krosh']
+
 }
-},
-methods: {
-add: function(){
-this.Smeshariki.push('Olga');
-},
-del: function(){
-this.Smeshariki.shift();
-},
-del1: function () {
-this.Smeshariki.pop();
-},
-splice: function(){
-this.Smeshariki.splice(2, 1);
-},
-sort: function(){
-this.Smeshariki.sort();
-},
-reverse: function () {
-    this.Smeshariki.reverse();
-},
 }
 }
 </script>
 
 <template>
-<ul>
-<li v-for="elem in Smeshariki">{{ elem }}</li>
-</ul>
-<button @click="add">1</button>
-<br>
-<button @click="del">2</button>
-<br>
-<button @click="del1">3</button>
-<br>
-<button @click="splice">4</button>
-<br>
-<button @click="sort">5</button>
-<br>
-<button @click="reverse">6</button>
+<p>Krosh</p>
+<p1>Pin</p1>
 </template>
 
 <style >
+p {
+		color: rgb(0, 255, 238);
+	}
+p1 {
+		color: rgb(0, 13, 255);
+	}
 </style>
