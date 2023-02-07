19136fccf4467e193988bf05c859651661c375db
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-07 21:13:49 +0300

    Task57

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,15 @@
 <script>
-export default{
-data(){
-return{
-    students: [
-    {
-        id: 1,
-        name: 'Polina',
-        surn: 'Shapkina',
-        salary: 100,
-				age: 30,
-    },
-    {
-        id: 2,
-        name: 'Diana',
-        surn: 'Kyzmenko',
-        salary: 200,
-				age: 40,
-    },
-    {
-        id: 3,
-        name: 'David',
-        surn: 'Avagyan',
-        salary: 300,
-				age: 50,
-    },
-    ],
-}
-},
-methods: {
-edit(student) {
-    student.isEdit = true;
-},
-save(student) {
-    student.isEdit = false;
-},
+import Smeshariki from './components/User.vue'
+export default {
+components: {
+    Smeshariki
 }
 }
 </script>
 
 <template>
-<table>
-<tr v-for="student in students" :key="student.id"> 
-<template v-if="!student.isEdit">
-{{ student.name }} {{ student.surn }} {{ student.salary }} {{ student.age }}<button class="button" @click="edit(student)">изменить</button>
-</template>
-<template v-else>
-<input v-model="student.name">
-<input v-model="student.surn">
-<input v-model="student.salary">
-<input v-model="student.age"><button class="button" @click="save(student)">сохранить</button>
-</template>
-</tr>
-</table>
+<Smeshariki />
 </template>
 
-<style scoped>
+<style>
 </style>
---
+++ src/components/User.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    data() {
+        return {
+name: "Krosh"
+        }
+    }
+}
+</script>
+
+<template>
+<p>{{ name }}</p>
+</template>
