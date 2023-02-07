082693d1dca35eda02f5cfb01808ffe2d9d44650
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-07 20:28:18 +0300

    Task51

--- src/App.vue
+++ src/App.vue
@@ -2,35 +2,33 @@
 export default{
 data(){
 return{
-    dayOfWeek: 'Понедельник',
-    a: ['Понедельник', 'Вторник','Среда', 'Четверг', 'Пятница','Суббота', 'Воскресенье'], 
-    day: '6',
-    b: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',' 31'],
-    month: 'Февраль',
-    v: ['Январь', 'Февраль','Март', 'Апрель', 'Май','Июнь', 'Июль', 'Август', 'Сентябрь','Октябрь', 'Ноябрь', 'Декабрь'],
-    year: '2023',
-    g: ['2023', '2022','2021', '2020', '2019','2018']
+    isDisabled: true,
+    isDisabled1: true,
+    checked: false,
 }
+},
+methods: {
+Smeshariki: function () {
+    this.isDisabled = false;
+},
+smeshariki: function () {
+    this.isDisabled1 = false;
+},
 }
 }
 </script>
 
 <template>
-    <select v-model="dayOfWeek">
-    <option v-for="a in a">{{ a }}</option>
-    </select>
+<input type="text" class="text-field__input"  v-bind:disabled="isDisabled">
 <br>
-<select v-model="day">
-<option v-for="b in b">{{ b }}</option>
-</select>
+<button @click="Smeshariki">Smeshariki</button>
 <br>
-<select v-model="month">
-<option v-for="v in v">{{ v }}</option>
-</select>
 <br>
-<select v-model="year">
-<option v-for="g in g">{{ g }}</option>
-</select>
+<br>
+<input type="text" class="text-field__input" v-bind:disabled="isDisabled1">
+<br>
+<input type="checkbox" v-model="checked" @click="smeshariki">
+<button @click="Smeshariki">Smeshariki</button> 
 </template>
 
 <style>
