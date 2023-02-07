a7c35de17bedf948057e681670309e7d36dacf9e
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-07 21:44:44 +0300

    Task62

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,37 @@ import Smeshariki from './components/User.vue'
 export default {
 data() {
     return {
-    name: 'Polina Shapkina',
-    salary: '25000',
-    age: '18',
-    }
+    smeshariki: [
+        {
+        id: 1,
+        name: 'Polina',
+        salary: 25000,
+        age: 18
+        },
+        {
+        id: 2,
+        name: 'Diana',
+        salary: 20000,
+        age: 19
+        },
+        {
+        id: 3,
+        name: 'David',
+        salary: 30000,
+        age: 19
+        },
+    ],
+}
 },
 components: {
     Smeshariki
 }
 }
+
 </script>
 
 <template>
-<Smeshariki :name="name" :salary="salary" :age="age"/>
+<Smeshariki v-for="smesharikis in smeshariki" :name="smesharikis.name" :salary="smesharikis.salary" :age="smesharikis.age" :key="smesharikis.id" />
 </template>
 <style>
 </style>
