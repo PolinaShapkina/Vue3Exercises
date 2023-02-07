cde35f30595107b0aeec592642f9d1f14cb4c1df
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-07 22:27:53 +0300

    Task69

--- src/components/User.vue
+++ src/components/User.vue
@@ -5,19 +5,34 @@ export default {
         name: String,
         surn: String,
     },
-    emits: ['remove'],
+    emits: ['change'],
     data() {
         return {
+            isEdit: false,
+            newName: this.name,
+            newSurn: this.surn,
         }
     },
     methods: {
+        edit(){
+            this.isEdit = true;
+        },
+        save(){
+            this.isEdit = false;
+            this.$emit('change', this.id, this.newName, this.newSurn);
+        }
     }
 }
 </script>
 
 <template>
+    <template v-if="!isEdit">
     <p>{{ name }} {{ surn }}</p>
-    <button @click="$emit('remove', id)">
-        remove
-    </button>
+    <button @click="edit">изменить</button>
+</template>
+    <template v-else>
+        <input class="text-field__input" v-model="newName">
+        <input class="text-field__input" v-model="newSurn">
+        <button @click="save">сохранить</button>
+    </template>
 </template>
