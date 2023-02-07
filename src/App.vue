31e044e0b0621aa904f40908fc4807f94e175203
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-07 22:22:51 +0300

    Task68

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <script>
 import Smeshariki from './components/User.vue'
 
+
 export default {
 data() {
     return {
-    
+    smeshariki: [
+        {
+        id: 1,
+        name: 'Pin',
+        surn: 'Smeshariki'
+        },
+        {
+        id: 2,
+        name: 'Nyusha',
+        surn: 'Smeshariki'
+        },
+        {
+        id: 3,
+        name: 'Krosh',
+        surn: 'Smeshariki'
+        },
+    ],
     }
 },
 components: {
     Smeshariki
 },
 methods: {
-    smesharikiName(name) {
-    console.log(name);
-    },
+    remove(id) {
+    this.smeshariki = this.smeshariki.filter((smesharikis) => {
+        return smesharikis.id !== id;
+    })
+    }
 }
 }
-
 </script>
 
 <template>
-<Smeshariki @show="smesharikiName"/>
+<Smeshariki v-for   ="smesharikis in smeshariki"
+		:id     ="smesharikis.id"
+		:name   ="smesharikis.name"
+		:surn   ="smesharikis.surn"
+		@remove ="remove"
+		:key    ="smesharikis.id"/>
 </template>
 <style>
 </style>
