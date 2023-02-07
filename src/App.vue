da1012374f6ffb5dd278f8ce46a16e26fea4fc58
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-07 20:33:29 +0300

    Task52

--- src/App.vue
+++ src/App.vue
@@ -2,33 +2,19 @@
 export default{
 data(){
 return{
-    isDisabled: true,
-    isDisabled1: true,
-    checked: false,
+Smeshariki: ' ',
+smeshariki: ' '
 }
-},
-methods: {
-Smeshariki: function () {
-    this.isDisabled = false;
-},
-smeshariki: function () {
-    this.isDisabled1 = false;
-},
 }
 }
 </script>
 
 <template>
-<input type="text" class="text-field__input"  v-bind:disabled="isDisabled">
+<input class="text-field__input" v-model="Smeshariki">
+<p>{{ Smeshariki }}</p>
 <br>
-<button @click="Smeshariki">Smeshariki</button>
-<br>
-<br>
-<br>
-<input type="text" class="text-field__input" v-bind:disabled="isDisabled1">
-<br>
-<input type="checkbox" v-model="checked" @click="smeshariki">
-<button @click="Smeshariki">Smeshariki</button> 
+<a href="#" class="inline-link-1" >Smeshariki</a>
+<p ></p>
 </template>
 
 <style>
